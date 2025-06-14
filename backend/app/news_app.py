668c4404d4b6ca192783598a7c15653a34f9426f
ariunaa <ajil.bur.ariunaa@gmail.com>
2025-06-14 13:47:44 +0900

    created project structure to python-backend, frontend

--- backend/app/news_app.py
+++ backend/app/news_app.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/hello")
 def hello():
     return {"message": "Hello form FastAPI"}
+
+def main():
+    print(f"somethings....")
+    
+
+if __name__ == "__main__":
+    main()
